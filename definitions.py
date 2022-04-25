bbb78f48e640ec9c0044963e9c3477352a8c3267
OneTonTyler <OneTonTyler@pm.me>
2022-04-25 14:15:23 -0600

    Fixed spelling

--- definitions.py
+++ definitions.py
@@ -5,7 +5,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Server Authentication
 # TODO set auth in config to grab credentials when necessary
-AUTH = {'USERNAME': 'TylerSingleton', 'PASSWORD': 'Foobar1234'}
+AUTH = {'USERNAME': 'TylerSingleton', 'PASSWORD': 'FooBar1234'}
 
 # FTP File
 # TODO this might need to be changed into a dictionary for future uses
