60e290139a108ff53ac5e29c5edcdb02854bd41a
OneTonTyler <OneTonTyler@pm.me>
2022-04-25 18:32:17 -0600

    Created a function to store the masked datafiles for easier processing. Will polished the DataFileExtraction class to handel more general use cases for other file extraction formats, and processing.

--- main.py
+++ main.py
@@ -90,8 +90,65 @@ class DataFileExtraction:
             print('\nDownloading files')
             ServerRequest(file_dir, urls, basename)
 
-    def save_data(self, file_type):
-        """Save the files to disk"""
+
+def save_data(path, file_type, mask):
+    """Save the files to disk"""
+    # Extract datasets
+    if file_type == 'SMAP':
+        # Extracting dataset
+        datasets = [h5py.File(dataset) for dataset in glob.glob(path)]
+
+        # TODO: Figure out how to pass this in as an argument
+        # Convert datasets into pandas dataframe object
+        df = [pd.DataFrame({
+            'Landcover_Class_0': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 0].ravel(),
+            'Landcover_Class_1': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 1].ravel(),
+            'Landcover_Class_2': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 2].ravel(),
+            'Soil_Moisture': dataset['Soil_Moisture_Retrieval_Data_AM']['soil_moisture'][:].ravel(),
+            'Latitude': dataset['Soil_Moisture_Retrieval_Data_AM']['latitude'][:].ravel(),
+            'Longitude': dataset['Soil_Moisture_Retrieval_Data_AM']['longitude'][:].ravel()
+        }) for dataset in datasets]
+        df = pd.concat(df, ignore_index=True)
+
+    elif file_type == 'CYGNSS':
+        # Extracting the dataset into memory using xarray for reading netcdf4
+        datasets = [xr.open_dataset(dataset) for dataset in glob.glob(path)]
+
+        # Convert into a Pandas DataFrame Object
+        # Longitude values go from 0 to 360, so 180 can be subtracted
+        df = [pd.DataFrame({
+            'SNR': dataset['ddm_snr'].values.ravel(),
+            'Latitude': dataset['sp_lat'].values.ravel(),
+            'Longitude': dataset['sp_lon'].values.ravel() - 180
+        }) for dataset in datasets]
+        df = pd.concat(df, ignore_index=True)
+
+    # Setting the latitude and longitude into point coordinates
+    # Must have a latitude and longitude pair
+    df['Coordinates'] = list(zip(df.Longitude, df.Latitude))
+    df['Coordinates'] = df['Coordinates'].apply(Point)
+
+    # Convert from Pandas Dataframe Object into a GeoPandas Dataframe Object
+    # Unknown values are masked as -9999
+    if file_type == 'SMAP':
+        gdf = geopandas.GeoDataFrame(df.query('Soil_Moisture >= 0'), geometry='Coordinates')
+    else:
+        gdf = geopandas.GeoDataFrame(df, geometry='Coordinates')
+
+    gdf = gdf.set_crs('epsg:4326')
+    gdf = gdf.drop_duplicates(subset='Coordinates')
+
+    # Clip Geopandas Dataframe with our mexico_mask
+    gdf = geopandas.clip(gdf, mask)
+    gdf = gdf.reset_index(drop=True)
+
+    # Save data geodataframe object to file
+    base_dir = os.path.join(r'Data_Files\Shape_Files', file_type)
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+
+    gdf.to_file(os.path.join(base_dir, file_type + '.shp'))
+
 
 # Create directory tree and download necessary files
 # DataFileExtraction()
@@ -104,65 +161,68 @@ shape_file = r'Data_Files\Shape_Files\world-administrative-boundaries.shp'
 world_boundaries = geopandas.read_file(shape_file)
 mexico_mask = world_boundaries['geometry'].loc[world_boundaries['name'] == 'Mexico']
 
+# Processing Files
+save_data(r'Data_Files\SMAP\*.h5', 'SMAP', mexico_mask)
+save_data(r'Data_Files\CYGNSS\*.nc', 'CYGNSS', mexico_mask)
 
-# ------------------------------------------------
-# TODO Save files to disk
-# TODO The following processes could be transformed into a class
-# Reading and processing SMAP files
-# Extracting the dataset into memory
-smap_files = r'Data_Files\SMAP\*.h5'
-smap = [h5py.File(dataset) for dataset in glob.glob(smap_files)]
-
-# TODO Extract the PM data as well
-# Converting dataset into a pandas dataframe object
-smap = [pd.DataFrame({
-    'Landcover_Class_0': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 0].ravel(),
-    'Landcover_Class_1': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 1].ravel(),
-    'Landcover_Class_2': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 2].ravel(),
-    'Soil_Moisture': dataset['Soil_Moisture_Retrieval_Data_AM']['soil_moisture'][:].ravel(),
-    'Latitude': dataset['Soil_Moisture_Retrieval_Data_AM']['latitude'][:].ravel(),
-    'Longitude': dataset['Soil_Moisture_Retrieval_Data_AM']['longitude'][:].ravel()
-}) for dataset in smap]
-smap = pd.concat(smap, ignore_index=True)
-
-# Setting the latitude and longitude into point coordinates
-smap['Coordinates'] = list(zip(smap.Longitude, smap.Latitude))
-smap['Coordinates'] = smap['Coordinates'].apply(Point)
-
-# Convert from Pandas Dataframe Object into a GeoPandas Dataframe Object
-# Unknown values are masked as -9999
-smap = geopandas.GeoDataFrame(smap.query('Soil_Moisture >= 0'), geometry='Coordinates')
-smap = smap.set_crs('epsg:4326')
-smap = smap.drop_duplicates(subset='Coordinates')
-
-# Clip Geopandas Dataframe with our mexico_mask
-smap = geopandas.clip(smap, mexico_mask)
-smap = smap.reset_index(drop=True)
-
-# ------------------------------------------------
-# Reading and processing CYGNSS files
-# Extracting the dataset into memory using xarray for reading netcdf4
-cygnss_files = r'Data_Files\CYGNSS\*.nc'
-cygnss = [xr.open_dataset(dataset) for dataset in glob.glob(cygnss_files)]
-
-# Convert into a Pandas DataFrame Object
-# Longitude values go from 0 to 360, so 180 can be subtracted
-cygnss = [pd.DataFrame({
-    'SNR': dataset['ddm_snr'].values.ravel(),
-    'Latitude': dataset['sp_lat'].values.ravel(),
-    'Longitude': dataset['sp_lon'].values.ravel() - 180
-}) for dataset in cygnss]
-cygnss = pd.concat(cygnss, ignore_index=True)
-
-# Setting the latitude and longitude into point coordinates
-cygnss['Coordinates'] = list(zip(cygnss.Longitude, cygnss.Latitude))
-cygnss['Coordinates'] = cygnss['Coordinates'].apply(Point)
-
-# Convert from Pandas Dataframe Object into a GeoPandas Dataframe Object
-cygnss = geopandas.GeoDataFrame(cygnss, geometry='Coordinates')
-cygnss = cygnss.set_crs('epsg:4326')
-
-# Clip Geopandas Dataframe with our mexico_mask
-cygnss = geopandas.clip(cygnss, mexico_mask)
-
-# ------------------------------------------------
+# # ------------------------------------------------
+# # TODO Save files to disk
+# # TODO The following processes could be transformed into a class
+# # Reading and processing SMAP files
+# # Extracting the dataset into memory
+# smap_files = r'Data_Files\SMAP\*.h5'
+# smap = [h5py.File(dataset) for dataset in glob.glob(smap_files)]
+#
+# # TODO Extract the PM data as well
+# # Converting dataset into a pandas dataframe object
+# smap = [pd.DataFrame({
+#     'Landcover_Class_0': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 0].ravel(),
+#     'Landcover_Class_1': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 1].ravel(),
+#     'Landcover_Class_2': dataset['Soil_Moisture_Retrieval_Data_AM']['landcover_class'][:, :, 2].ravel(),
+#     'Soil_Moisture': dataset['Soil_Moisture_Retrieval_Data_AM']['soil_moisture'][:].ravel(),
+#     'Latitude': dataset['Soil_Moisture_Retrieval_Data_AM']['latitude'][:].ravel(),
+#     'Longitude': dataset['Soil_Moisture_Retrieval_Data_AM']['longitude'][:].ravel()
+# }) for dataset in smap]
+# smap = pd.concat(smap, ignore_index=True)
+#
+# # Setting the latitude and longitude into point coordinates
+# smap['Coordinates'] = list(zip(smap.Longitude, smap.Latitude))
+# smap['Coordinates'] = smap['Coordinates'].apply(Point)
+#
+# # Convert from Pandas Dataframe Object into a GeoPandas Dataframe Object
+# # Unknown values are masked as -9999
+# smap = geopandas.GeoDataFrame(smap.query('Soil_Moisture >= 0'), geometry='Coordinates')
+# smap = smap.set_crs('epsg:4326')
+# smap = smap.drop_duplicates(subset='Coordinates')
+#
+# # Clip Geopandas Dataframe with our mexico_mask
+# smap = geopandas.clip(smap, mexico_mask)
+# smap = smap.reset_index(drop=True)
+#
+# # ------------------------------------------------
+# # Reading and processing CYGNSS files
+# # Extracting the dataset into memory using xarray for reading netcdf4
+# cygnss_files = r'Data_Files\CYGNSS\*.nc'
+# cygnss = [xr.open_dataset(dataset) for dataset in glob.glob(cygnss_files)]
+#
+# # Convert into a Pandas DataFrame Object
+# # Longitude values go from 0 to 360, so 180 can be subtracted
+# cygnss = [pd.DataFrame({
+#     'SNR': dataset['ddm_snr'].values.ravel(),
+#     'Latitude': dataset['sp_lat'].values.ravel(),
+#     'Longitude': dataset['sp_lon'].values.ravel() - 180
+# }) for dataset in cygnss]
+# cygnss = pd.concat(cygnss, ignore_index=True)
+#
+# # Setting the latitude and longitude into point coordinates
+# cygnss['Coordinates'] = list(zip(cygnss.Longitude, cygnss.Latitude))
+# cygnss['Coordinates'] = cygnss['Coordinates'].apply(Point)
+#
+# # Convert from Pandas Dataframe Object into a GeoPandas Dataframe Object
+# cygnss = geopandas.GeoDataFrame(cygnss, geometry='Coordinates')
+# cygnss = cygnss.set_crs('epsg:4326')
+#
+# # Clip Geopandas Dataframe with our mexico_mask
+# cygnss = geopandas.clip(cygnss, mexico_mask)
+#
+# # ------------------------------------------------
